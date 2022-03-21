84635464976bb72450d3152ef08097966b3b0a4c
feriwnarta <feriwnarta26@gmail.com>
2022-03-21 16:53:07 +0700

    update tempat nulis status dan status bar yang berwarna lightblue

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:aplikasi_rw/screen/home_page_screen.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -57,6 +58,12 @@ class _TemplateScreenState extends State<TemplateScreen> {
 
   @override
   Widget build(BuildContext context) {
+    
+
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.lightBlue
+    ));
+
     heightTabBar = MediaQuery.of(context).size.height * 0.2;
 
     return Scaffold(
